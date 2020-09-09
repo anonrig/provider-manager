e6e9235c4b86263b74e8ae767b684f0b0bb79ed8
Yagiz Nizipli <yagiznizipli@me.com>
2020-09-09 10:50:20 +0300

    fix(spm): make package dynamically linked

--- Package.swift
+++ Package.swift
@@ -11,18 +11,13 @@ let package = Package(
     .watchOS(.v5)
   ],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "ProviderManager",
+      type: .dynamic,
       targets: ["ProviderManager"]),
   ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-  ],
+  dependencies: [],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "ProviderManager",
       dependencies: [])
